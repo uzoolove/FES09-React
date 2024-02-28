f22c13a78c7c9b46f95dfa2f0ccd983e86bea2f4
Kilyong Jeong <uzoolove@gmail.com>
2024-02-28 10:18:48 +0900

    ✨ 완료

--- workspace-ins/web-example/ch01/counter/03/Mylib.js
+++ workspace-ins/web-example/ch01/counter/03/Mylib.js
@@ -1,8 +1,34 @@
 const Mylib = (() => {
-  const createElement = (tag, props, children) => {
-    
+  const createElement = (tag, props, ...children) => {
+    // 요소 노드 생성
+    const elem = document.createElement(tag);
+
+    // 속성 추가
+    if(props){
+      for(const attrName in props){
+        const value = props[attrName];
+        elem.setAttribute(attrName, value);
+      }
+    }
+
+    // 자식 요소 추가
+    for(let child of children){
+      if(typeof child === 'string' || typeof child === 'number'){
+        child = document.createTextNode(child);
+      }
+      elem.appendChild(child);
+    }
+
+    return elem;
   };
   return { createElement };
 })();
+
+// const Mylib = {
+//   createElement: ()=> (tag, props, children) => {
+    
+//   }
+// };
+
 
 export default Mylib;
