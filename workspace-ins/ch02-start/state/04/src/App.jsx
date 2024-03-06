c6800cfe53bf32ab931367406d27deb48f76d269
Kilyong Jeong <uzoolove@gmail.com>
2024-03-06 14:48:49 +0900

    🚧

---
+++ workspace-ins/ch02-start/state/04/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import AddressBookEdit from "./components/AddressBookEdit";
+
+function App(){
+  const [user, setUser] = useState({
+    "_id": 4,
+    "email": "u1@market.com",
+    "name": "데이지",
+    "phone": "01044445555",
+    "address": "서울시 강남구 논현동 222",
+    "type": "user",
+    "createdAt": "2024.01.25 21:08:14",
+    "updatedAt": "2024.02.04 09:38:14",
+    "extra": {
+      "birthday": "11-30",
+      "membershipClass": "MC02",
+      "addressBook": [
+        {
+          "id": 1,
+          "name": "회사",
+          "value": "서울시 강동구 천호동 123"
+        },
+        {
+          "id": 2,
+          "name": "집",
+          "value": "서울시 강동구 성내동 234"
+        }
+      ]
+    }
+  });
+
+  const handleAddressChange = e => {
+
+  };
+
+  return (
+    <>
+      <h1>상태 관리 - 회원 정보 수정</h1>
+      <p>
+        이메일: { user.email }<br/>
+        이름: { user.name }<br/>
+        전화번호: { user.phone }<br/>
+      </p>
+      <ul>
+        { user.extra.addressBook?.map(address => {
+          return <li key={ address.id }>{ address.name }: { address.value }</li>;
+        }) }
+      </ul>
+
+      <p>
+        <AddressBookEdit 
+          addressBook={ user.extra.addressBook } 
+          handleAddressChange={ handleAddressChange }
+        />
+      </p>
+    </>
+  );
+}
+
+export default App;
