a478663d7763c9bf72fca617642bf843d7824456
Kilyong Jeong <uzoolove@gmail.com>
2024-03-12 11:34:16 +0900

    🚧

--- workspace-ins/ch03-hooks/06-useCallback/src/App.jsx
+++ workspace-ins/ch03-hooks/06-useCallback/src/App.jsx
@@ -8,10 +8,17 @@ function App(){
   const [quantity, setQuantity] = useState(1);
   const [shippingFees, setShippingFees] = useState(defaultShippingFees);
 
+  // 수량이 변경될 때 배송비 다시 계산
+  const handleChange = e => {
+    const newQuantity = e.target.value;
+    setShippingFees(defaultShippingFees * Math.ceil(newQuantity / 5));
+    setQuantity(newQuantity);
+  };
+
   const handleClick = () => {
     alert(`${price * quantity + shippingFees}원 결제 하시겠습니까?`);
     // 결제 로직...
-    
+
   };
 
   return (
@@ -23,7 +30,8 @@ function App(){
       <div>
         가격: { price }원<br/>
         수량: <input type="number" min="1" value={ quantity } 
-          onChange={ e => setQuantity(e.target.value) } />
+          onChange={ handleChange } /><br/>
+        상품 가격: { price * quantity }<br/>
       </div>
 
       <Payment price={ price } quantity={ quantity } 
--- workspace-ins/ch03-hooks/06-useCallback/src/Payment.jsx
+++ workspace-ins/ch03-hooks/06-useCallback/src/Payment.jsx
@@ -1,19 +1,15 @@
 import PropTypes from 'prop-types';
 
 Payment.propTypes = {
-  price: PropTypes.number.isRequired,
-  quantity: PropTypes.number.isRequired,
   shippingFees: PropTypes.number.isRequired,
   handleClick: PropTypes.func.isRequired,
 };
 
-function Payment({ price, quantity, shippingFees, handleClick }){
+function Payment({ shippingFees, handleClick }){
   return (
     <>
-      <h2>금액</h2>
-      <div>상품 가격: { price * quantity }</div>
+      <h2>금액</h2>      
       <div>배송비: { shippingFees }</div>
-      <div>결제 예정 금액: { price * quantity + shippingFees }</div>
       <button type="button" onClick={ handleClick }>결제</button>
     </>
   );
