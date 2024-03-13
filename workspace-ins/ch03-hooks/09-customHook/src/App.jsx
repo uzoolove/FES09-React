e0edc13b8c874dd2749ef5678d77c253c8332c42
Kilyong Jeong <uzoolove@gmail.com>
2024-03-13 15:43:20 +0900

    ✨

--- workspace-ins/ch03-hooks/09-customHook/src/App.jsx
+++ workspace-ins/ch03-hooks/09-customHook/src/App.jsx
@@ -1,11 +1,13 @@
 import { ReactCsspin } from 'react-csspin';
+// import useFetch from "./hooks/useFetch";
+import useAxios from './hooks/useAxios';
 
-import useFetch from "./hooks/useFetch";
+import 'react-csspin/dist/style.css';
 
 function App(){
 
-  const { isLoading, data, error } = useFetch({
-    url: '/todolist?delay=2000'
+  const { isLoading, data, error } = useAxios({
+    url: '/todolist?delay=1000'
   });
   
   return (
@@ -14,7 +16,7 @@ function App(){
       <h2>할일 목록</h2>
 
       { isLoading &&
-        <ReactCsspin color="black" />
+        <ReactCsspin message="로딩중..." />
       }
 
       { error && 
