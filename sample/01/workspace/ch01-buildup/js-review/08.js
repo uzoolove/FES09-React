823ffed6cfacb93eed6b5c1701743794a7f554c9
Kilyong Jeong <uzoolove@gmail.com>
2024-03-13 09:59:15 +0900

    🧪

--- sample/01/workspace/ch01-buildup/js-review/08.js
+++ sample/01/workspace/ch01-buildup/js-review/08.js
@@ -1,17 +1,26 @@
 // 지정한 수가 소수인지 여부를 반환
 var isPrime = function(num){
-  
+  console.time('소요 시간');
+  console.log('소수 판별 시작.', num);
+
+  // TODO: 소수 판별 코드
+  let prime = true;
+
+
+  console.log('소수 판별 결과.', prime);
+  console.timeEnd('소요 시간');
+  return prime;
 };
 
-console.time('소요시간');
-console.log('3 -> ', isPrime(3));
-console.log('4 -> ', isPrime(4));
-console.log('5 -> ', isPrime(5));
-console.log('6 -> ', isPrime(6));
-console.log('7 -> ', isPrime(7));
-console.log('8 -> ', isPrime(8));
-console.log('9 -> ', isPrime(9));
-console.log('1000000007 -> ', isPrime(1000000007));
-console.log('1000000007 -> ', isPrime(1000000007));
-console.log('1000000007 -> ', isPrime(1000000007));
-console.timeEnd('소요시간');
+isPrime(1);
+isPrime(2);
+isPrime(3);
+isPrime(4);
+isPrime(5);
+isPrime(6);
+isPrime(7);
+isPrime(8);
+isPrime(9);
+isPrime(1000000007);
+isPrime(1000000007);
+isPrime(1000000007);
