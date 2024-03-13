7fa553ebbffc6bf6cc8081e35a5562d2edf89ace
Kilyong Jeong <uzoolove@gmail.com>
2024-03-13 14:25:04 +0900

    🚧

--- workspace-ins/ch03-hooks/08-customHook/src/App.jsx
+++ workspace-ins/ch03-hooks/08-customHook/src/App.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
+
+const API_SERVER = 'https://todo-api.frontendschool.shop/api';
+
 function App(){
+  const [data, setData] = useState(null);
+
+  const request = async params => {
+    try{
+      const res = await fetch(API_SERVER + params.url);
+      console.log(res);
+      const jsonData = await res.json();
+      console.log('jsonData', jsonData);
+      if(jsonData.ok){
+        // setData(jsonData);
+      }else{
+        throw new Error(jsonData.error.message);
+      }
+    }catch(err){
+      // 에러 처리
+      console.error(err.message);
+    }
+  };
+
+  const fetchParams = { url: '/todolistsdfgsdfsdsdf' };
+  console.log('api 서버 호출', fetchParams);
+  request(fetchParams);
+
   return (
     <>
       <h1>Custom Hook - fetch API 사용</h1>
       <h2>할일 목록</h2>
-      <ul>
-        <li>Todo 끝내기</li>
-      </ul>
+      { data && (
+        <ul>
+          { data.items?.map(item => <li key={ item._id }>{ item.title }</li>) }
+        </ul>
+      ) }
     </>
   );
 }
