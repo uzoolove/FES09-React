219b6338d0661603967c8993d9861b3a7a5eea5d
Kilyong Jeong <uzoolove@gmail.com>
2024-03-14 10:05:28 +0900

    🚧

---
+++ workspace-ins/ch07-router/01/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Header';
+import Home from './Home';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+
+function App(){
+  return (
+    <>
+      <Header />
+      <Home />
+      <Page1 />
+      <Page2 />
+    </>
+  );
+}
+
+export default App;
---
+++ workspace-ins/ch07-router/01/src/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+  return (
+    <header>
+      <a href="home.html">home</a>
+      <br/>
+      <a href="page1.html">page1</a>
+      <br/>
+      <a href="page2.html">page2</a>
+    </header>
+  );
+}
+
+export default Header;
