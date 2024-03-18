bbec17be0765521320ce764958c51dcc36a94cfa
Kilyong Jeong <uzoolove@gmail.com>
2024-03-18 15:21:59 +0900

    🚧

--- workspace-ins/app/todolist/04-router/src/pages/TodoEdit.jsx
+++ workspace-ins/app/todolist/04-router/src/pages/TodoEdit.jsx
@@ -1,22 +1,53 @@
-import { Link } from "react-router-dom";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
 
 function TodoEdit(){
+  const { _id } = useParams();
+  const axios = useAxiosInstance();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (formData) => {
+    try{
+      await axios.patch(`/todolist/${ _id }`, formData);
+      alert('할일이 수정 되었습니다.');
+    }catch(err){
+      console.error(err);
+      alert('할일 수정에 실패했습니다.');
+    }
+  };
+
   return (
     <div id="main">
       <h2>할일 수정</h2>
       <div className="todo">
-        <form>
+        <form onSubmit={ handleSubmit(onSubmit) }>
           <label htmlFor="title">제목 :</label>
-          <input type="text" id="title" value="할일1" autoFocus />
+          <input 
+            type="text" 
+            id="title" 
+            autoFocus 
+            { ...register('title', { required: '제목을 입력하세요.' }) }
+          />
           <br/>
           <label htmlFor="content">내용 :</label>
-          <input type="text" id="content" value="내용1" />
+          <textarea 
+            id="content" 
+            cols="25" 
+            rows="8" 
+            { ...register('content', { required: '내용을 입력하세요.' }) }
+          />
           <br/>
           <label htmlFor="done">완료 :</label>
-          <input type="checkbox" id="done" checked />
+          <input 
+            type="checkbox" 
+            id="done" 
+            checked 
+            { ...register('done') }
+          />
           <br/>
-          <Link to="/detail">수정</Link>
-          <Link to="/detail">취소</Link>
+          <button type="submit">수정</button>
+          <button type="reset">취소</button>
         </form>
       </div>
     </div>
--- workspace-ins/ch01-buildup/js-review/06-05.js
+++ workspace-ins/ch01-buildup/js-review/06-05.js
@@ -29,18 +29,40 @@ function f2(f1Result){
   });
 }
 
+async function f1(){
+  console.log(`2. f1 작업 시작.`);
+  console.log(`3. f1 작업중...`);
+  // ......
+  console.log(`4. f1 작업 종료.`);
+  return (`f1의 결과물`);
+}
+
+async function f2(f1Result){
+  console.log(`5. ${f1Result}로 f2 작업 시작.`);
+  console.log(`6. f2 작업중...`);
+  // ......
+  console.log(`7. f2 작업 종료.`);
+  return (`최종 결과물`);
+}
+
 async function test(){
   // ES2017 async/await
   try{
     const f1Result = await f1(); // 동기함수처럼 동작
     const f2Result = await f2(f1Result);
     console.log('8.', f2Result);
+    return 'test 완료.';
   }catch(err){
     console.error(err);
   }
 }
 
 console.log('1. 테스트 시작.');
-test();
+
+(async () => {
+  const result = await test();
+  console.log(result);
+})();
+
 console.log('9. 테스트 완료.');
 
