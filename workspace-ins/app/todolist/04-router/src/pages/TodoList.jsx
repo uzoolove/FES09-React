64ddc9ff00a6c431931af55d408797b1ae25d22f
Kilyong Jeong <uzoolove@gmail.com>
2024-03-18 16:30:49 +0900

    ✨

--- workspace-ins/app/todolist/04-router/src/pages/TodoList.jsx
+++ workspace-ins/app/todolist/04-router/src/pages/TodoList.jsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
-import useAxios from '@hooks/useAxios';
-import TodoListItem from "./TodoListItem";
-import { ReactCsspin } from 'react-csspin';
-import 'react-csspin/dist/style.css';
+import TodoListItem from "@pages/TodoListItem";
 import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useEffect, useState } from "react";
 
 function TodoList(){
   const axios = useAxiosInstance();
+  // 할일 목록
+  const [data, setData] = useState();
   
-  const { isLoading, data, error } = useAxios({
-    url: '/todolist?delay=1000'
-  });
+  const fetchList = async () => {
+    const response = await axios.get('/todolist');
+    setData(response.data);
+  };
+
+  // 마운트 되면 최초 한번 목록 조회
+  useEffect(() => {
+    fetchList();
+  }, []);
 
   const handleDelete = async _id => {
     try{
       await axios.delete(`/todolist/${ _id }`);
       alert('할일이 삭제 되었습니다.');
       // API 서버에서 목록 조회
-      
+      fetchList();
     }catch(err){
       console.error(err);
       alert('할일 삭제에 실패했습니다.');
@@ -26,20 +32,9 @@ function TodoList(){
 
   const itemList = data?.items.map(item => <TodoListItem key={ item._id } item={ item } handleDelete={ handleDelete } />);
 
-
-
   return (
     <div id="main">
       <h2>할일 목록</h2>
-
-      { isLoading &&
-        <ReactCsspin message="로딩중..." />
-      }
-
-      { error && 
-        <p style={{ color: 'red' }}>{ error.message }</p>
-      }
-      
       <div className="todo">
         <Link to="/add">추가</Link>
         <br/>
