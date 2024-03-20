115b912df9fd77f0021ef2d09613a4f9836e2127
Kilyong Jeong <uzoolove@gmail.com>
2024-03-20 10:30:20 +0900

    🧪

--- workspace-ins/ch06-contextapi/after/src/components/Left2.jsx
+++ workspace-ins/ch06-contextapi/after/src/components/Left2.jsx
@@ -1,13 +1,17 @@
 import Left3 from '@components/Left3';
-import { useEffect } from 'react';
+import CounterContext from '@context/CounterContext';
+import { useContext, useEffect } from 'react';
 
 function Left2() {
   useEffect(()=>{
     console.log('### Left2 렌더링.');
   });
+
+  const { state: { count } } = useContext(CounterContext);
+
   return (
     <div>
-      <h1>Left2</h1>
+      <h1>Left2 : { count }</h1>
       <Left3 />
     </div>
   );
