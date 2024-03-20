f7dbf8fda6bd4689e1c3df085bd93b1b75edcb1a
Kilyong Jeong <uzoolove@gmail.com>
2024-03-20 15:26:43 +0900

    🚧

--- workspace-ins/ch08-globalstate/zustand/src/components/Right3.jsx
+++ workspace-ins/ch08-globalstate/zustand/src/components/Right3.jsx
@@ -1,13 +1,22 @@
+import useCounterStore from '@zustand/counter.mjs';
 import { useEffect } from 'react';
 
 function Right3() {
   useEffect(()=>{
     console.log('#### Right3 렌더링.');
   });
+
+  // const { countUp, countDown } = useCounterStore();
+
+  // 렌더링 최적화를 위해서 수동으로 필요한 속성만 반환받아서 사용
+  const countUp = useCounterStore(state => state.countUp);
+  const countDown = useCounterStore(state => state.countDown);
+
   return (
     <div>
       <h1>Right3</h1>
-      <button onClick={ () => {} }>+</button>
+      <button onClick={ () => countDown(2) }>-</button>
+      <button onClick={ () => countUp(1) }>+</button>
     </div>
   );
 }
