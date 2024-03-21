164b9f44111bdc4ae2812a14b42dd42cb7c149d4
Kilyong Jeong <uzoolove@gmail.com>
2024-03-21 14:03:02 +0900

    🚧

--- workspace-ins/ch13-skeleton/board-app/src/pages/BoardResult.jsx
+++ workspace-ins/ch13-skeleton/board-app/src/pages/BoardResult.jsx
@@ -1,5 +1,25 @@
-function BoardResult(){
+import Footer from "@components/layout/Footer";
+import Header from "@components/layout/Header";
 
+function BoardResult(){
+  return (
+    <>
+      <Header />
+
+      <div>
+        <section>
+          <p>등록되었습니다.</p>				
+          <hr />
+          <div>
+            <a href="board/3">글확인</a>
+            <a href="board/">목록</a>
+          </div>
+        </section>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default BoardResult;
