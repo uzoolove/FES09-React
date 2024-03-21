4d0117724d84c5894021f4299492b963e010290e
Kilyong Jeong <uzoolove@gmail.com>
2024-03-21 16:11:25 +0900

    🚧

---
+++ workspace-ins/ch13-skeleton/board-app/src/pages/BoardListItem.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+BoardListItem.propTypes = {
+  item: PropTypes.object.isRequired
+};
+
+function BoardListItem({ item }){
+  return (
+    <tr>
+      <td>{ item._id }</td>
+      <td><Link to="/boards/1">{ item.title }</Link></td>
+      <td>{ item.user.name }</td>
+      <td>30</td>
+      <td>{ item.updatedAt }</td>
+    </tr>
+  );
+}
+
+export default BoardListItem;
