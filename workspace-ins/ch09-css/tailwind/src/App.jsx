5c87a2e3110293789205a0557be46f85a1a6fa0d
Kilyong Jeong <uzoolove@gmail.com>
2024-03-25 14:50:41 +0900

    🚧

--- workspace-ins/ch09-css/tailwind/src/App.jsx
+++ workspace-ins/ch09-css/tailwind/src/App.jsx
@@ -6,6 +6,9 @@ function App(){
       <h1 className="myTitle">Tailwind CSS</h1>
       <p>Hello Tailwind</p>
       <Button>기본 버튼</Button>
+      <Button bgColor="black">검은 버튼</Button>
+      <Button bgColor="blue">파란 버튼</Button>
+      <Button bgColor="red">빨간 버튼</Button>
     </>
   );
 }
--- workspace-ins/ch09-css/tailwind/src/components/Button.jsx
+++ workspace-ins/ch09-css/tailwind/src/components/Button.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 
 Button.propTypes = {
-  children: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired,
+  bgColor: PropTypes.string,
 };
 
-function Button({ children }){
-  return <button>{ children }</button>
+function Button({ children, bgColor }){
+
+  return <button className={`bg-${bgColor}-500 text-white font-semibold px-2 py-1 ml-2 text-base hover:bg-blue-600 rounded`}>{ children }</button>
 }
 
 export default Button;
