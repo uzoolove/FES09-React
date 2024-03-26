6d2c2ce6fb0832a8e8a1e55fa3bd7e91cb413e83
Kilyong Jeong <uzoolove@gmail.com>
2024-03-26 09:17:07 +0900

    ✨

--- workspace-ins/ch13-skeleton/board-app/src/components/Button.jsx
+++ workspace-ins/ch13-skeleton/board-app/src/components/Button.jsx
@@ -4,9 +4,10 @@ Button.propTypes = {
   children: PropTypes.string.isRequired,
   bgColor: PropTypes.string,
   size: PropTypes.string,
+  type: PropTypes.string,
 };
 
-function Button({ children, bgColor='blue', size='md', ...rest }){
+function Button({ children, type="button", bgColor='blue', size='md', ...rest }){
   let btnColor = {
     gray: `bg-gray-500`,
     blue: 'bg-blue-500',
@@ -18,7 +19,7 @@ function Button({ children, bgColor='blue', size='md', ...rest }){
     lg: 'py-2 px-6 text-lg',
   };
 
-  return <button className={`${ btnColor[bgColor] } ${ btnSize[size] } text-white font-semibold ml-2 text-base hover:bg-blue-600 rounded`} { ...rest }>{ children }</button>
+  return <button type={ type } className={`${ btnColor[bgColor] } ${ btnSize[size] } text-white font-semibold ml-2 text-base hover:bg-blue-600 rounded`} { ...rest }>{ children }</button>
 }
 
 export default Button;
