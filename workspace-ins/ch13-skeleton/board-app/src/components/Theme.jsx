cbad646dbfe6dd99f7811bd9ea1289546f308a1a
Kilyong Jeong <uzoolove@gmail.com>
2024-03-26 14:10:36 +0900

    ✨

--- workspace-ins/ch13-skeleton/board-app/src/components/Theme.jsx
+++ workspace-ins/ch13-skeleton/board-app/src/components/Theme.jsx
@@ -1,5 +1,7 @@
+import useThemeStore from "@zustand/themeStore.mjs";
+
 function Theme(){
-  const isDarkMode = true;
+  const { isDarkMode, toggleTheme } = useThemeStore();
 
   const moonHidden = isDarkMode ? '' : 'hidden';
   const sunHidden = isDarkMode ? 'hidden' : '';
@@ -8,7 +10,7 @@ function Theme(){
     <button 
       type="button"
       data-toggle-dark="dark" 
-      // onClick={ toggleTheme }
+      onClick={ toggleTheme }
       className="ml-4 flex items-center w-8 h-8 justify-center text-xs font-medium text-gray-700 bg-white border border-gray-200 rounded-lg toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
       <svg 
         data-toggle-icon="moon" 
