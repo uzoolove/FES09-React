c5334212c0acbbd5b44a0a781340a899ad1c75cf
Kilyong Jeong <uzoolove@gmail.com>
2024-03-26 09:50:08 +0900

    🚧

--- workspace-ins/ch13-skeleton/board-app/src/components/layout/Header.jsx
+++ workspace-ins/ch13-skeleton/board-app/src/components/layout/Header.jsx
@@ -15,32 +15,31 @@ function Header(){
   const [user, setUser] = useRecoilState(memberState);
 
   return (
-    <header>
-      <nav>
-        <div>
-          <a href="/">
-            <img src="/vite.svg" alt="로고 이미지" />
-            <span>게시판</span>
+    <header className="min-w-80 bg-gray-200 text-gray-800">
+      <nav className="flex flex-wrap justify-center items-center p-4 md:flex-nowrap md:justify-between">
+        <div className="w-1/2 order-1 md:w-auto">
+          <a className="flex items-center gap-2" href="/">
+            <img className="mr-3 h-6" src="/vite.svg" alt="로고 이미지" />
+            <span className="text-xl font-semibold">게시판</span>
           </a>
         </div>
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/boards">정보 공유</Link></li>
-            <li><Link to="/boards">자유 게시판</Link></li>
-            <li><Link to="/boards">질문 게시판</Link></li>
+        <div className="w-auto order-2 text-lg mt-4 md:mt-0">
+          <ul className="flex items-center gap-6 uppercase">
+            <li><Link to="/boards">정보공유</Link></li>
+            <li><Link to="/boards">자유게시판</Link></li>
+            <li><Link to="/boards">질문게시판</Link></li>
           </ul>
         </div>
-        <div>
+        <div className="w-1/2 order-1 flex justify-end items-center md:order-2 md:w-auto">
           { user ? (
             <p>
               { user.name }님 :)
               <button type="button" onClick={ handleLogout }>로그아웃</button>
             </p>
           ) : (
-            <div>
-              <Button onClick={ () => navigate('/users/login') }>로그인</Button>
-              <Button bgColor="gray" onClick={ () => navigate('/users/signup') }>회원가입</Button>
+            <div className="flex justify-end">
+              <Button size="sm" onClick={ () => navigate('/users/login') }>로그인</Button>
+              <Button size="sm" bgColor="gray" onClick={ () => navigate('/users/signup') }>회원가입</Button>
             </div>
           ) }
         </div>
