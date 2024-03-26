e394e9aa70aa54e179672ab7d1679dafebc51196
Kilyong Jeong <uzoolove@gmail.com>
2024-03-26 11:40:11 +0900

    ✨

--- workspace-ins/ch13-skeleton/board-app/src/components/layout/index.jsx
+++ workspace-ins/ch13-skeleton/board-app/src/components/layout/index.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 function Layout(){
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
       <Outlet />
       <Footer />
--- workspace-ins/ch13-skeleton/board-app/src/pages/user/Login.jsx
+++ workspace-ins/ch13-skeleton/board-app/src/pages/user/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
   const setUser = useSetRecoilState(memberState);
   const axios = useCustomAxios();
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors }  } = useForm({
+  const { register, handleSubmit, formState: { errors }, setError } = useForm({
     values: {
       email: 'aa@bb.cc',
       password: '********'
@@ -28,50 +28,57 @@ function Login() {
       });
       alert(res.data.item.name + '님 로그인 되었습니다.');
       navigate('/'); // 메인페이지로 이동
-    } catch (err) {
-      alert(err.response?.data.message);
+    } catch (err) { // AxiosError(네트워크 에러-response가 없음, 서버의 4xx, 5xx 응답 상태 코드를 받았을 때-response 있음)
+      if(err.response?.data.errors){  // API 서버가 응답한 에러
+        err.response?.data.errors.forEach(error => setError(error.path, { message: error.msg }) );
+      }else if(err.response?.data.message){
+        alert(err.response?.data.message);
+      }
     }
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <h2>로그인</h2>
+    <div className="min-w-80 flex-grow flex items-center justify-center">
+      <div className="p-8 shadow-md rounded-lg w-full max-w-md dark:bg-gray-600">
+        <div className="text-center py-4">
+          <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">로그인</h2>
         </div>
+
         <form onSubmit={ handleSubmit(onSubmit) }>
-          <div>
-            <label htmlFor="email">이메일</label>
+          <div className="mb-4">
+            <label className="block text-gray-700 dark:text-gray-200 font-bold mb-2" htmlFor="email">이메일</label>
             <input 
               type="email" 
               id="email" 
               placeholder="이메일을 입력하세요"       
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 dark:bg-gray-700"      
               { ...register('email', {
                 required: '이메일을 입력하세요.',
                 pattern: {
                   value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                   message: '이메일 형식이 아닙니다.'
                 } 
-              })}
+              }) }
             />
-            { errors.email && <p>{ errors.email.message }</p>}
+            { errors.email && <p className="ml-2 mt-1 text-sm text-red-500">{ errors.email.message }</p> }
           </div>
-          <div>
-            <label htmlFor="password">비밀번호</label>
+          <div className="mb-4">
+            <label className="block text-gray-700 dark:text-gray-200 font-bold mb-2" htmlFor="password">비밀번호</label>
             <input 
               type="password" 
               id="password" 
               placeholder="비밀번호를 입력하세요" 
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 dark:bg-gray-700" 
               { ...register('password', {
-                required: "비밀번호를 입력하세요."
-              })} 
+                required: "비밀번호를 입력하세요.",
+              }) } 
             />
-            { errors.password && <p>{ errors.password.message }</p>}
-            <Link to="#">비밀번호를 잊으셨나요?</Link>
+            { errors.password && <p className="ml-2 mt-1 text-sm text-red-500">{ errors.password.message }</p> }
+            <Link className="block mt-6 ml-auto text-gray-500 dark:text-gray-300 hover:underline" to="#">비밀번호를 잊으셨나요?</Link>
           </div>
-          <div>
+          <div className="mt-14 flex justify-center items-center">
             <Submit>로그인</Submit>
-            <Link to="/users/signup">회원가입</Link>
+            <Link className="ml-8 text-blue-500 hover:underline" to="/users/signup">회원가입</Link>
           </div>
         </form>
       </div>
