762f3409116cfba0a896b3cb75061a180efa9c77
Kilyong Jeong <uzoolove@gmail.com>
2024-03-28 14:42:29 +0900

    ✨ 무한 스크롤 추가

--- workspace-ins/ch13-skeleton/board-app/src/pages/board/ReplyList.jsx
+++ workspace-ins/ch13-skeleton/board-app/src/pages/board/ReplyList.jsx
@@ -2,7 +2,9 @@ import useCustomAxios from "@hooks/useCustomAxios.mjs";
 import ReplyItem from "@pages/board/ReplyItem";
 import ReplyNew from "@pages/board/ReplyNew";
 import { useParams } from "react-router-dom";
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroller";
 
 
 // import { useEffect, useState } from "react";
@@ -25,19 +27,39 @@ function ReplyList(){
   // }, []);
 
 
-  const { data } = useQuery({
+  const { data, fetchNextPage } = useInfiniteQuery({
     queryKey: ['posts', _id, 'replies'],
-    queryFn: () => axios.get(`/posts/${ _id }/replies`, { params: { sort: JSON.stringify({ _id: -1 }) } }),
-    select: response => response.data,
+    queryFn: ({ pageParam=1 }) => axios.get(`/posts/${ _id }/replies?delay=3000`, { params: { page: pageParam, limit: import.meta.env.VITE_REPLY, sort: JSON.stringify({ _id: -1 }) } }),
+    // select: response => response.data,
     // refetchInterval: 1000
+    // 마지막 페이지와 함께 전체 페이지 목록을 받아서 queryFn에 전달할 pageParam 값을 리턴하도록 구현한다.
+    // false를 리턴하면 더이상 queryFn이 호출되지 않고 무한 스크롤 종료
+    // lastPage는 res.data
+    getNextPageParam: (lastPage, allPages) => {
+      console.log("lastPage", lastPage, "allPages", allPages);
+      const totalPages = lastPage.data.pagination.totalPages;
+      const nextPage = allPages.length < totalPages ? allPages.length + 1 : false;
+      return nextPage;
+    }
   });
 
-  const list = data?.item.map(item => <ReplyItem key={ item._id } item={ item } />);
+  // const list = data?.item.map(item => <ReplyItem key={ item._id } item={ item } />);
+  // ES2019 Array.prototype.flatMap();
+  // 2차원 배열을 1차원 배열로 변환
+  console.log('data', data);
+  const list = data?.pages?.flatMap(page => page.data.item.map(item => <ReplyItem key={ item._id } item={ item } />));
+  
+
+  console.log('list', list);
+  const hasNext = data?.pages.at(-1).data.pagination.page < data?.pages.at(-1).data.pagination.totalPages;
 
   return (
     <section className="mb-8">
       <h4 className="mt-8 mb-4 ml-2">댓글 { list?.length || 0 }개</h4>
-      { list }
+
+      <InfiniteScroll pageStart={1} loadMore={ fetchNextPage } hasMore={ hasNext } loader={ <div>로딩중...</div> }>
+        { list || [] }
+      </InfiniteScroll>
 
       <ReplyNew />
 
